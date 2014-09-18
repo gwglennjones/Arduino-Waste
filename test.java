c25c394c5163b5f95e0d3bc76608b69036ea480d
AZAlexDay <adday4@asu.edu>
2014-09-18 11:28:42 -0700

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
-public class main{
-	public static void main(String []args){
+public class test
+{
+	public static void main(String []args)
+	{
 		System.out.println("please work");
 		System.out.println("EXPLOSIONS");
 	}
